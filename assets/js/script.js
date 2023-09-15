44e799537a1afed84411ace91bb29a11a0ac9cca
BrianPizz <bpizzimentijr@outlook.com>
2023-09-14 23:27:15 -0400

    setup game prompt and guess button event listener

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var guessNum = $('guess-number').val();
+var guessBtnEl = $('#guess-button');
+var feedbackEl = $('#feedback');
+
+var randomNum = Math.floor(Math.random() * 100);
+
+
+
+guessBtnEl.on('click', function(){
+
+})
